951114457678e93733d7f6d56b888d5f486034ae
Timur <timbro2312@gmail.com>
2026-09-07 00:54:20 +0100

    Ебля не такая сложная

--- src/anonchatbot/window/linux.py
+++ src/anonchatbot/window/linux.py
@@ -1,6 +1,6 @@
-import Xlib
+import Xlib.display
 
-
+from Xlib import X
 def detect_session_type():
     pass
 
@@ -8,4 +8,20 @@ def detect_session_type():
 def find_window():
     display = Xlib.display.Display()
     screen = display.screen()
-    root = displa
+    root = screen.root
+    atom = display.intern_atom("_NET_CLIENT_LIST")
+    full_geometry = root.get_full_property(
+        atom, 
+        X.AnyPropertyType
+    )
+    print(full_geometry)
+"""    for window in windows:
+        title = window.get_wm_name()
+        if title is None:
+            continue
+        if type(title) == bytes:
+            title = title.decode()
+        title_lower = title.lower()
+        if "telegram" in title_lower or "ayugramdesktop" in title_lower:
+            print(window.get_geometry())"""
+find_window()
