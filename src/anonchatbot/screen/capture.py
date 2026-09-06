6e027542c7f5ba389d47a462ff369cdeb1087efb
Timur <timbro2312@gmail.com>
2026-09-06 22:16:35 +0100

    Back-up #2

--- src/anonchatbot/screen/capture.py
+++ src/anonchatbot/screen/capture.py
@@ -4,18 +4,29 @@ from PIL import Image
 
 def capture_monitor(area:tuple[int, int, int, int], 
     monitor_id: int = 1    
-) -> Image.Image:
+) -> None:
+    # Take screenshot in selected region
     with mss.MSS() as sct:
         monitor = sct.monitors[monitor_id]
-        region = {"left":area[0],
-                 "top":area[1],
-                 "width":area[2],
-                 "height":area[3]
+        region = {
+            "left":area[0],
+            "top":area[1],
+            "width":area[2],
+            "height":area[3]
+        }
+        absolute_area = {
+            "left": monitor["left"] + region["left"],
+            "top": monitor["top"] + region["top"],
+            "width": region["width"],
+            "height": region["height"]
         }
 
-        print(region)
-        print(monitor)
+        screenshot = sct.grab(absolute_area)
 
-    return Image.Image()
+    # Covnert mss.screenshot.ScreenShot to PIL.Image for further processing
+    screenshot_PIL = screenshot.to_pil()
+    Image.Image.show(screenshot_PIL)
 
-capture_monitor((200,200,600, 67))
+    return
+
+capture_monitor((200,200,600, 67), 2)
