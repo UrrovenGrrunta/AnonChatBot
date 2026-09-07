02760a360c1764c8b52a2338634abfd767586103
UrrovenGrrunta <timbro2312@gmail.com>
2026-09-07 14:16:39 +0100

    Separated logic for X11 and Wayland

--- src/anonchatbot/window/linux.py
+++ src/anonchatbot/window/linux.py
@@ -1,27 +1,47 @@
 import Xlib.display
+import os
 
 from Xlib import X
+
+
 def detect_session_type():
-    pass
+    sesstion_type = os.getenv("XDG_SESSION_TYPE")
+    backend = None
+    match sesstion_type:
+        case "x11":
+            backend = X11Backend()
+            backend.find_window()
+        case "wayland":
+            backend = WaylandBackend()
+            backend.find_window()
+        case _:
+            raise RuntimeError("Can not identify session type.")
 
 
-def find_window():
-    display = Xlib.display.Display()
-    screen = display.screen()
-    root = screen.root
-    atom = display.intern_atom("_NET_CLIENT_LIST")
-    full_geometry = root.get_full_property(
-        atom, 
-        X.AnyPropertyType
-    )
-    print(full_geometry)
-"""    for window in windows:
-        title = window.get_wm_name()
-        if title is None:
-            continue
-        if type(title) == bytes:
-            title = title.decode()
-        title_lower = title.lower()
-        if "telegram" in title_lower or "ayugramdesktop" in title_lower:
-            print(window.get_geometry())"""
-find_window()
+class X11Backend:
+    def find_window(self):
+        display = Xlib.display.Display()
+        screen = display.screen()
+        root = screen.root
+        atom = display.intern_atom("_NET_CLIENT_LIST")
+        full_geometry = root.get_full_property(
+            atom, 
+            X.AnyPropertyType
+        )
+        print(full_geometry)
+    """    for window in windows:
+            title = window.get_wm_name()
+            if title is None:
+                continue
+            if type(title) == bytes:
+                title = title.decode()
+            title_lower = title.lower()
+            if "telegram" in title_lower or "ayugramdesktop" in title_lower:
+                print(window.get_geometry())"""
+
+
+class WaylandBackend:
+    def find_window(self):
+        print("Hello from Wayland!")
+
+detect_session_type()
